a55ce11739c6487eb20118e061a5723a37c94683
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2018-01-22 17:47:06 +0100

    1. version done

--- QuickShifter/quickshifter.ino
+++ QuickShifter/quickshifter.ino
@@ -1,9 +1,30 @@
+#include <Arduino.h>
+
+#define BUTTON 2
+#define LED LED_BUILTIN
+
+extern HardwareSerial Serial;
+
+int buttonState = 0;
+unsigned long previousMillis = 0;
+const int interval = 60;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
   Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(millis());
+  buttonState = digitalRead(BUTTON);
+  if (!buttonState)
+    shift();
+}
+
+//TODO: Dauerkontakt fixen...
+void shift(){
+  previousMillis = millis();
+  digitalWrite(LED, HIGH);
+  while(millis() - previousMillis < interval){}
+  digitalWrite(LED, LOW);
 }
