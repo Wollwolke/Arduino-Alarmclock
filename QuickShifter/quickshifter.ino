4e81a6a793f322362d3af0ba7d78013b85a26af3
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2018-01-22 22:21:29 +0100

    fix for holding the button

--- QuickShifter/quickshifter.ino
+++ QuickShifter/quickshifter.ino
@@ -5,9 +5,11 @@
 
 extern HardwareSerial Serial;
 
-int buttonState = 0;
-unsigned long previousMillis = 0;
 const int interval = 60;
+
+int buttonState = 0;
+int wasRealesed = 0;
+unsigned long previousMillis = 0;
 
 void setup() {
   pinMode(BUTTON, INPUT_PULLUP);
@@ -16,8 +18,10 @@ void setup() {
 }
 
 void loop() {
-  buttonState = digitalRead(BUTTON);
-  if (!buttonState)
+  // may not recognice to short releases...
+  if(1 == digitalRead(BUTTON))
+    wasRealesed = 1;
+  if (!digitalRead(BUTTON) && wasRealesed)
     shift();
 }
 
@@ -27,4 +31,10 @@ void shift(){
   digitalWrite(LED, HIGH);
   while(millis() - previousMillis < interval){}
   digitalWrite(LED, LOW);
+
+  // prevent continuous execution
+  wasRealesed = 0;
+
+  // "debouncing"
+  delay(500);
 }
