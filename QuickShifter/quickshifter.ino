13efbe668773712c148ef0d6ee0d46cf9761e26a
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2018-01-22 22:36:47 +0100

    removed Serial

--- QuickShifter/quickshifter.ino
+++ QuickShifter/quickshifter.ino
@@ -14,18 +14,15 @@ unsigned long previousMillis = 0;
 void setup() {
   pinMode(BUTTON, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
-  Serial.begin(115200);
 }
 
 void loop() {
-  // may not recognice to short releases...
   if(1 == digitalRead(BUTTON))
     wasRealesed = 1;
   if (!digitalRead(BUTTON) && wasRealesed)
     shift();
 }
 
-//TODO: Dauerkontakt fixen...
 void shift(){
   previousMillis = millis();
   digitalWrite(LED, HIGH);
